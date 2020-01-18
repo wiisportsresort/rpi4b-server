9eb534aa497ecf20eed91827ef6e2ef3fc631a72
wiisportsresorts <ccs009013@gmail.com>
2020-01-17 23:33:28 -0500

    add https support

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-const express = require("express");
-const app = express();
-app.use(express.static("public"));
+const fs = require('fs')
+const https = require('https')
+const express = require("express")
+
+const app = express()
+
+// Certificate
+const privateKey  = fs.readFileSync('/etc/letsencrypt/live/wiisportsresorts.dev/privkey.pem', 'utf8')
+const certificate = fs.readFileSync('/etc/letsencrypt/live/wiisportsresorts.dev/cert.pem',    'utf8')
+const ca          = fs.readFileSync('/etc/letsencrypt/live/wiisportsresorts.dev/chain.pem',   'utf8')
+
+const credientials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+}
+
+app.use(express.static("public"))
 
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
-});
+  response.sendFile(__dirname + "/views/index.html")
+})
 
-const listener = app.listen(80, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+const httpsServer = https.createServer(credientials, app)
+
+const listener = httpsServer.listen(443, function() {
+  console.log("Your app is listening on port " + listener.address().port)
 });
 
